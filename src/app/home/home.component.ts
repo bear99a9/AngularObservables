3695e0084264253de22567e3462802c46a12aa8f
Sean Edwards <sean.edwards@teachingpersonnel.com>
2022-11-10 16:40:50 +0000

    Pipe operator with map

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription, Observable } from "rxjs";
+import { map } from 'rxjs/operators';
 
-import { interval, Subscription, Observable, observable } from "rxjs";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, OnDestroy{
+export class HomeComponent implements OnInit, OnDestroy {
 
-    private firstObsSubscription: Subscription;
+  private firstObsSubscription: Subscription;
   constructor() { }
 
   ngOnInit() {
@@ -18,35 +19,40 @@ export class HomeComponent implements OnInit, OnDestroy{
 
     const customIntervalObservable = Observable.create(observer => {
       let count = 0;
-        setInterval(()=> {
-          observer.next(count);
-          if (count == 3) {
-            observer.complete();
-          }
-          if (count > 3) {
-            observer.error(new Error('Count is greater than 3'));
-            
-          }
-          count++;
-        }, 1000);
+      setInterval(() => {
+        observer.next(count);
+        if (count == 3) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+
+        }
+        count++;
+      }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe({
+    this.firstObsSubscription = customIntervalObservable.pipe(map(
+      (currentCount: number) => {
+        return 'Round: ' + (currentCount + 1);
+      }
+    )).subscribe({
       next: (currentCount: number) => {
-        console.log(currentCount)},
-        error: (error: any) => {
-          console.log(error);
-          alert(error.message);
-        },
-        complete: () => {
-          alert('Observable has completed');
-          console.log('Completed');
-        }
+        console.log(currentCount)
+      },
+      error: (error: any) => {
+        console.log(error);
+        alert(error.message);
+      },
+      complete: () => {
+        alert('Observable has completed');
+        console.log('Completed');
+      }
     });
   }
 
   ngOnDestroy(): void {
-    
+
     this.firstObsSubscription.unsubscribe();
   }
 }
